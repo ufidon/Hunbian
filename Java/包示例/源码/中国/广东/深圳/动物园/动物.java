5c28a0f7000be7c9d4d78b110f301cb6e1f267a8
ufidon <ufidon@gmail.com>
2016-11-10 16:21:32 -0600

    java 包示例

---
+++ Java/包示例/源码/中国/广东/深圳/动物园/动物.java
@@ -0,0 +1,7 @@
+/* File name : Animal.java */
+package 中国.广东.深圳.动物园;
+
+interface 动物 {
+   public void 吃();
+   public void 跑();
+}
---
+++ Java/包示例/源码/中国/广东/深圳/动物园/哺乳动物.java
@@ -0,0 +1,23 @@
+package 中国.广东.深圳.动物园;
+/* File name : MammalInt.java */
+
+public class 哺乳动物 implements 动物 {
+
+   public void 吃() {
+      System.out.println("哺乳动物喝奶");
+   }
+
+   public void 跑() {
+      System.out.println("哺乳动物爱跑路");
+   } 
+
+   public int 腿数() {
+      return 4;
+   }
+
+   public static void main(String args[]) {
+      哺乳动物 我家小牛 = new 哺乳动物();
+      我家小牛.吃();
+      我家小牛.跑();
+   }
+} 
