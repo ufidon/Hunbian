11aae5b509bbc5c166953f412402864c1679a361
ufidon <ufidon@gmail.com>
2017-06-03 15:47:50 -0500

    控制台中文输出

--- C/控制台中文输出/linconsole.cpp
+++ C/控制台中文输出/linconsole.cpp
@@ -12,7 +12,7 @@ int main()
 	const	wchar_t* a = L"千山鸟飞绝，万径人踪灭！";
 
 	setlocale(LC_CTYPE, "");
-	wprintf(L"alpha is:\n\t%ls %lc\n", a, a[3]);      // 中文输出正常
+	wprintf(L"中文字符串及索引:\n\t%ls %lc\n", a, a[3]);      // 中文输出正常
 
   return 0;
 }
